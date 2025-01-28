eca3bb78d075db45e38a61dda50aacb6d65ee23f
adam hoban <s00253815@atu.ie>
2025-01-28 15:57:23 +0000

    yy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,CommonModule], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'worksheet2';
+
+  thingsToDo = [
+    "Learn Javascript",
+    "Learn Angular", 
+    "Learn Redux"
+    ]
+
+    thingsCompleted=[];
+
+    summary():string {
+      return '${this.thingsToDo.length} to do / ${this.thingsCompleted.length} done';
+    }
 }
